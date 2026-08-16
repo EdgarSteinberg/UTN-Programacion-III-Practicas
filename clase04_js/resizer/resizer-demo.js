dc5d9afb966eeabd1e95b9a244423bd5c407aa92
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-08-16 15:13:02 -0300

    manejando dinamicamente css con js

--- clase04_js/resizer/resizer-demo.js
+++ clase04_js/resizer/resizer-demo.js
@@ -1,15 +1,65 @@
 const panelWidthControl = document.querySelector('#panel-width');
 const panelWidthValue = document.querySelector('#panel-width-value');
 
-if (panelWidthControl && panelWidthValue) {
-  const setLeftPanelWidth = (value) => {
-    const widthInPx = `${value}px`;
-    document.documentElement.style.setProperty('--left-panel-width', widthInPx);
-    panelWidthValue.textContent = widthInPx;
-  };
+const redInput = document.querySelector('#red');
+const greenInput = document.querySelector('#green');
+const blueInput = document.querySelector('#blue');
 
-  panelWidthControl.addEventListener('input', (event) => {
-    const nextValue = Number(event.target.value);
-    setLeftPanelWidth(nextValue);
-  });
+let red = 31;
+let green = 34;
+let blue = 40;
+
+
+if (panelWidthControl && panelWidthValue) {
+
+    const setLeftPanelWidth = (value) => {
+        const widthInPx = `${value}px`;
+
+        document.documentElement.style.setProperty(
+            '--left-panel-width',
+            widthInPx
+        );
+
+        panelWidthValue.textContent = widthInPx;
+    };
+
+    panelWidthControl.addEventListener('input', (event) => {
+        const nextValue = Number(event.target.value);
+
+        setLeftPanelWidth(nextValue);
+    });
 }
+
+
+const setBackgroundColor = () => {
+
+    document.documentElement.style.setProperty(
+        '--left-panel-background-color',
+        `rgb(${red}, ${green}, ${blue})`
+    );
+
+};
+
+
+redInput.addEventListener('input', (event) => {
+    red = Number(event.target.value);
+
+    setBackgroundColor();
+});
+
+
+greenInput.addEventListener('input', (event) => {
+    green = Number(event.target.value);
+
+    setBackgroundColor();
+});
+
+
+blueInput.addEventListener('input', (event) => {
+    blue = Number(event.target.value);
+
+    setBackgroundColor();
+});
+
+
+setBackgroundColor();
