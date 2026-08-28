795e540d38417617fd8dc709536f18f3d830c5ce
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-08-27 21:40:55 -0300

    primer contador en angular

--- angular/chat-app/src/app/app.module.ts
+++ angular/chat-app/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ContadorComponent } from './contador/contador.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, ContadorComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/chat-app/src/app/contador/contador.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  standalone: false,
+  templateUrl: './contador.component.html',
+  styleUrl: './contador.component.css'
+})
+export class ContadorComponent {
+  valorContador: number = 0;
+
+  incrementar() {
+    this.valorContador++;
+  }
+
+  decrementar() {
+    this.valorContador--;
+  }
+}
