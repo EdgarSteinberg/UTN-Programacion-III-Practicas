7023cadfa1e4fe2f0cee28f6ce20316cd011e307
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-09-09 16:59:48 -0300

    consumiento api rest mas navegacion entre componentes

--- angular/chat-app/src/app/product-detail/product-detail.component.ts
+++ angular/chat-app/src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Product, productList } from '../products/products.mock';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Iproduct } from '../models/product.model';
+import { ApiService } from '../services/api.service';
+/* import { Product, productList } from '../products/products.mock'; */
 
 @Component({
   selector: 'app-product-detail',
@@ -10,24 +12,49 @@ import { Product, productList } from '../products/products.mock';
 })
 export class ProductDetailComponent implements OnInit {
 
-  product?: Product;
+  product?: Iproduct;
   /*   color: string = ''; */
-  productList: Product[] = productList
+  /*  productList: Iproduct[] = [] */
   loading: boolean = true;
   color: string = '';
 
-  constructor(private _route: ActivatedRoute) { }
+  constructor(
+    private _route: ActivatedRoute,
+    private _apiService: ApiService
+  ) { }
+
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this._route.params.subscribe(params => {
-        /* console.log(params['productId']); */
-        /* this.producto = params['productId']; */
-        /* this.color = params['category']; */
-        this.product = this.productList.find(product => product.id == params['productId']);
-        this.color = this.product?.price as number > 5 ? 'red' : '';
-        this.loading = false;
-      })
-    }, 1500);
+    this._route.params.subscribe({
+      next: (params: Params) => {
+        this._apiService.getProductById(Number(params['productId'])).subscribe({
+          next: (data: Iproduct) => {
+            this.product = data;
+
+            // Casteo/comparación del precio
+            this.color = this.product.price > 5 ? 'red' : '';
+
+            this.loading = false;
+          },
+          error: (error: any) => {
+            console.log(error);
+            
+          }
+        });
+      }
+    });
   }
+
+  /*  ngOnInit(): void {
+     setTimeout(() => {
+       this._route.params.subscribe(params => {
+         console.log(params['productId']); 
+          this.producto = params['productId']; 
+         this.color = params['category']; 
+         this.product = this.productList.find(product => product.id == params['productId']);
+         this.color = this.product?.price as number > 5 ? 'red' : '';
+         this.loading = false;
+       })
+     }, 1500);
+   } */
 }
--- angular/chat-app/src/app/products/products.component.ts
+++ angular/chat-app/src/app/products/products.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { productList } from './products.mock';
+import { Component, OnInit } from '@angular/core';
+/* import { productList } from './products.mock'; */
+import { Iproduct } from '../models/product.model';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-products',
@@ -7,7 +9,17 @@ import { productList } from './products.mock';
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
-  
-   productList = productList;
+export class ProductsComponent implements OnInit {
+
+  productList: Iproduct[] = [];
+
+  constructor(private _apiService: ApiService) { }
+
+
+  ngOnInit(): void {
+    this._apiService.getAllProducts().subscribe((data: Iproduct[]) => {
+       
+      this.productList = data;
+    })
+  }
 }
